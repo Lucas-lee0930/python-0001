8760622d1def00a629931a7d00d95dbfd369f4fa
Lucas-lee0930 <23524035@hanmail.net>
2025-12-30 16:14:09 +0900

    ggg

--- venv/sample.py
+++ venv/sample.py
@@ -1,3 +1,11 @@
 file_name = "main.py"
-file_name_list = file_name[-1]
-print(file_name_list)
+file_name_to_four = file_name[:4]
+file_name_to_four = file_name[4:]
+file_name_all = file_name[:]
+s = "python"
+print(s[0])
+print(s[1])
+print(s[-1])
+print(s[-2])
+print(s[0:3])
+print(s[:3])
